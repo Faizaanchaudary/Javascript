f900055e047d2cd5ee1cb16181d46a63c5cc8410
Faizaanchaudary <faizaanchaudary@gmail.com>
2021-12-26 17:33:58 +0500

    l

---
+++ tut53.js
@@ -0,0 +1,19 @@
+console.log("This is tutorial 53");
+function greet (Name, Greetext)
+{console.log(Greettext + " " Name)
+
+{console.log( Name + " is a good Boy")
+}
+let Name = "Harry";
+let Name1 = "Ali";
+let Name2 = "Ahmed";
+let Name3 = "Ahsan";
+let Greetext= "GoodMorning"
+greet(Name1, greettext);
+greet(Name2, greettext);
+greet(Name3, greettext);
+greet(Name, greettext);
+// console.log( Name + " is a good Boy");
+// console.log( Name1 + " is a good Boy");
+// console.log( Name2 + " is a good Boy");
+// console.log( Name3 + " is a good Boy");
