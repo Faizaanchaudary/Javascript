d40fea365b31987d9d035da916373da38b63cb42
Faizan Abbas <96379311+Faizaanchaudary@users.noreply.github.com>
2023-04-05 16:00:26 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var _ = require ('underscore');
-
-
-var result = _.contains(['ali' , 'ahmed' , 'aryan'] , 'ali');
-console.log(result);
+module.exports.table = function(x){
+    for(i=0; i<=10; i++){
+        console.log(`${x} x ${i} = ${x*i}`);
+    }
+    return ('');
+};
